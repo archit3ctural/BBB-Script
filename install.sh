6d21cbda9c35b37c1da2da96d0a041a3454fc68f
archit3ctural <105637001+archit3ctural@users.noreply.github.com>
2023-06-12 20:44:54 +0100

    Update install.sh 12/06/23

--- install.sh
+++ install.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
+# Run apt-get update & upgrade
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-sudo apt-get install -y python3-pip
-sudo apt-get install -y python-pip
-sudo apt-get install -y ruby-full
-sudo apt-get install -y git
-sudo apt-get install -y xargs
-sudo apt-get install -y jq
-sudo apt-get install -y nmap
-sudo apt-get install -y golang
-
-# Move Go to $PATH environment
-sudo mv go /usr/local
+# Install and move Go to $PATH environment
+wget https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz
+sudo tar -C /usr/local/ -xzf go1.13.5.linux-amd64.tar.gz
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
@@ -21,6 +14,31 @@ echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
 echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH\n' >> ~/.bash_profile	
 source ~/.bash_profile
+
+# Install other dependencies
+sudo apt-get install -y jq
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y rename
+
+
+# Install GoSpider
+GO111MODULE=on go install github.com/jaeles-project/gospider@latest
+
+# Install Hakrawler
+GO111MODULE=on go install github.com/hakluke/hakrawler@latest
+
+# Install SubDomainizer
+TBC
+
+# Install Subscraper
+git clone https://github.com/m8sec/subscraper
+cd subscraper
+python3 setup.py install
+
+# Install Amass
+GO111MODULE=on go install -v github.com/owasp-amass/amass/v3/...@master
+
 
 # Add nahamsec aliases to bash_profile
 echo "installing bash_profile aliases from recon_profile"
@@ -33,46 +51,6 @@ cd ..
 sudo rm -r recon_profile
 echo "done"
 
-# Create a tools folder in ~/
-mkdir ~/tools
-cd ~/tools/
 
-# Install subfinder
-echo "installing subfinder"
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-echo "done"
-
-# Install WPScan
-echo "installing wpscan"
-sudo gem install wpscan
-echo "done"
-
-# Install dirsearch
-echo "installing dirsearch"
-git clone https://github.com/maurosoria/dirsearch.git
-cd ~/tools/
-echo "done"
-
-# Install amass
-echo "installing amass"
-go install -v github.com/owasp-amass/amass/v3/...@master
-echo "done"
-
-# Install massdns
-echo "installing massdns"
-git clone https://github.com/blechschmidt/massdns.git
-cd ~/tools/massdns
-make
-cd ~/tools/
-echo "done"
-
-# Install httprobe
-echo "installing httprobe"
-go get -u github.com/tomnomnom/httprobe 
-echo "done"
-
-# Clear up
-cd ~/
-sudo rm -r BBB-Script.git
 
 
